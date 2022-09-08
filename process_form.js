60b3101786f07b52891900b28039e15672d6a66a
quentingourier <73183451+quentingourier@users.noreply.github.com>
2022-09-08 18:18:57 +0200

    Add files via upload

--- process_form.js
+++ process_form.js
@@ -1,5 +1,5 @@
 //Contact Form in PHP
-const form = document.querySelector("form");
+const form = document.querySelector("form"),
 statusTxt = form.querySelector(".button-area span");
 form.onsubmit = (e)=>{
   e.preventDefault();
